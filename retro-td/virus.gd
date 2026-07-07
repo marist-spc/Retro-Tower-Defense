d5d29bc1b00554c5d947009df1131e77d871b68f
ThatOneCoolDude24 <grhack243@outlook.com>
2026-07-07 12:20:24 -0400

    health and speed vars

--- retro-td/virus.gd
+++ retro-td/virus.gd
@@ -11,25 +11,39 @@ var brown=6
 var gray=7
 
 var health
+var speed 
 
+var redHealth=1
+var blueHealth=2
+var greenHealth=4
+var yellowHealth=8
+var pinkHealth=16
+var brownHealth=32
+var grayHealth=100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if colorType==red:
 		$AnimatedSprite2D.play("red-walk")
-		
+		health=redHealth
 	elif colorType==blue:
 		$AnimatedSprite2D.play("blue-walk")
+		health=blueHealth
 	elif colorType==green:
 		$AnimatedSprite2D.play("green-walk")
+		health=greenHealth
 	elif colorType==yellow:
 		$AnimatedSprite2D.play("yellow-walk")
+		health=yellowHealth
 	elif colorType==pink:
 		$AnimatedSprite2D.play("pink-walk")
+		health=pinkHealth
 	elif colorType==brown:
 		$AnimatedSprite2D.play("brown-walk")
+		health=brownHealth
 	elif colorType==gray:
 		$AnimatedSprite2D.play("gray-walk")
+		health=grayHealth
 	
 
 
