a6d78728172004e72206ae4e61b5d6f7c9a6e8dd
ThatOneCoolDude24 <grhack243@outlook.com>
2026-07-07 13:38:01 -0400

    did thingie with speed

--- retro-td/virus.gd
+++ retro-td/virus.gd
@@ -11,7 +11,7 @@ var brown=6
 var gray=7
 
 var health
-var speed 
+var speed
 
 var redHealth=1
 var blueHealth=2
@@ -21,30 +21,44 @@ var pinkHealth=16
 var brownHealth=32
 var grayHealth=100
 
+var redSpeed=1
+var blueSpeed=1.1
+var greenSpeed=2
+var yellowSpeed=1.5
+var pinkSpeed=2
+var brownSpeed=1
+var graySpeed=.5
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if colorType==red:
 		$AnimatedSprite2D.play("red-walk")
 		health=redHealth
+		speed=redSpeed
 	elif colorType==blue:
 		$AnimatedSprite2D.play("blue-walk")
 		health=blueHealth
+		speed=blueSpeed
 	elif colorType==green:
 		$AnimatedSprite2D.play("green-walk")
 		health=greenHealth
+		speed=greenSpeed
 	elif colorType==yellow:
 		$AnimatedSprite2D.play("yellow-walk")
 		health=yellowHealth
+		speed=yellowSpeed
 	elif colorType==pink:
 		$AnimatedSprite2D.play("pink-walk")
 		health=pinkHealth
+		speed=pinkSpeed
 	elif colorType==brown:
 		$AnimatedSprite2D.play("brown-walk")
 		health=brownHealth
+		speed=brownSpeed
 	elif colorType==gray:
 		$AnimatedSprite2D.play("gray-walk")
 		health=grayHealth
-	
+		speed=graySpeed
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
