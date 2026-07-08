3f97e4a1fc02c34982d5ccdf9f0ff8fbf64d8ea5
ThatOneCoolDude24 <grhack243@outlook.com>
2026-07-08 15:54:34 -0400

    did stuff with hud

--- retro-td/hud.gd
+++ retro-td/hud.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
-var current_score = 500
 
+@export var cryptoScreen: Label
+@export var securityScreen: Label
+
+var current_score = 500
 var security=100
 
 func add_points(amount: int):
 	current_score += amount
+	
+	cryptoScreen.text="Crypto: "
 	
 var amount = 1
 
@@ -12,5 +17,5 @@ var amount = 1
 func _on_endpoint_body_entered(body: Node2D) -> void:
 	security-=body.health
 	
-	print(security)
 	
+	securityScreen.text="Security: "
