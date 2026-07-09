6df782699cf52df5ec6116d2fef6961493d8278a
CrunchTime52t <Sladerjlogan@gmail.com>
2026-07-09 15:51:02 -0700

    Im so sorry, also was Game Hero supposed to be so expensive?

---
+++ retro-td/Tower.gd
@@ -0,0 +1,16 @@
+extends AnimatedSprite2D
+
+@export var cost = 250
+
+@export var upgrade_costs = {
+"path1": [500,750, 1000],
+ "path2": [600, 800, 1200] }
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
